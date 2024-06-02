e25790c0ba4aa9fbc0dd13b588fb3b86d6fb84dc
J.P. <145814971+ramackersjp@users.noreply.github.com>
2024-06-03 01:47:26 +0200

    Init

---
+++ login.py
@@ -0,0 +1,13 @@
+while True:
+    email = input("Enter email: ")
+    if email == "ramackersjp@outlook.com":
+        print("Address found")
+        break
+    else:
+        print("Address not found, please try again.")
+
+password = input("Enter password: ")
+if password == "1234":
+    print("Welcome")
+else:
+    print("Wrong")
